d4885ca974f8c3a2f203a4ec1e278113c58813db
bryant <bryantkrause@gmail.com>
2021-05-03 14:56:12 -0700

    ad idd

--- models/Budget.js
+++ models/Budget.js
@@ -1,10 +1,14 @@
+const opts = { toJSON: { virtuals: true } };
+
 module.exports = (model, Schema) => {
     const Budget = new Schema({
       name: String,
+      id: String,
       expense: String,
       expAmt: Number,
       User: { type: Schema.Types.ObjectId, ref: 'User' },
-    }, { timestamps: { createdAt: 'birthday', updatedAt: 'pcChange' } })
+    }, { timestamps: { createdAt: 'birthday', updatedAt: 'pcChange' } },
+    opts)
   
     return model('Budget', Budget)
   }
